6ae52de8fef6f05e59b8a6136adcde60295ec76e
Romero, Reinard Adrian <reinard.a.romero@accenture.com>
2021-11-12 11:30:21 +0800

    Final Test

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("-----***** Test *****-----");
+        System.out.println("-----***** This is my final test *****-----");
     }
 }
